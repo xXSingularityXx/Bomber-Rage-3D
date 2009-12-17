15baa68fa2e05c63b483c02e6b2825a51f59d9cc
U-MATSCHULAT\Samuel <Samuel@matschulat.(none)>
2009-12-17 03:53:40 -0200

    forgot file

---
+++ modelLoader.hpp
@@ -0,0 +1,11 @@
+#ifndef _BOMBER_MODELLOADER
+#define _BOMBER_MODELLOADER
+
+#include "modelLoader/ImageLoader.h"
+#include "modelLoader/Vector3D.h"
+#include "modelLoader/ObjectManager.h"
+#include "modelLoader/Node.h"
+#include "modelLoader/StaticNode.h"
+#include "modelLoader/Texture.h"
+
+#endif
